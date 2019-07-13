cf046e75f453cf4621219b1609b28b47c9e644b1
Austin Alber <austinalber@gmail.com>
2019-07-13 09:15:55 -0400

    Added dynamic buttons to page through javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,72 @@
 // JavaScript Goes Here
 
-// Event listener for all button elements
-$("buttons").on("click", function() {
-    // Grab and store value of button
-    var selection = $(this).attr("data-value")
-});
+// Variable Declaration
+var arr = [];
+var count = 0;
 
-$("run-search").on("click", function()  {
+// Functions
+function displayGifInfo()   {
+    var selection = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    selection + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response)  {
+        // JSON gif grabing
+        console.log(response);
+        
+        // $("#gifs-here").text(JSON.stringify(response));
+
+
+
+    })
+}
+
+function renderButtons()    {
+    // Delete Prior Buttons to prevent repeats
+    $("#gif-buttons").empty();
+
+    // Loop Through the array of Gifs
+    for(var i = 0; i < arr.length; i++) {
+        var a = $("<button>");
+        a.addClass("gif");
+        a.attr("data-name", arr[i]);
+        a.text(arr[i]);
+        $("#gif-buttons").append(a);
+    }
+}
+
+$("#run-submit").on("click", function()  {
     // When 'Search' is clicked: Find and Post gifs
     event.preventDefault();
 
-    var = 
+    // Get value from prompt box
+    var selection = $("#prompt-term").val().trim();
+
+    // Push result into an array
+    arr.push(selection);
+    console.log(arr);
+
+    renderButtons();
+
+    // // Create a new variable to become button
+    // var selectionButton = $("<button>");
+    // selectionButton.attr("id", "button-" + count);
+    // selectionButton.text(selection);
+
+    // // Add button to the 'gifs-here' div
+    // $("#gifs-here").append(selectionButton);
+
+    // // Clear Promptbox when done
+    // $("#prompt-term").val("");
+
+    // // Add to the count
+    // count++;
 });
+
+// 
+$(document).on("click", ".gif-buttons", displayGifInfo);
+
+renderButtons();

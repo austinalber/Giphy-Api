efca8b4deb8574e4aa5e12b0fd86f36c24d5b0c9
Austin Alber <austinalber@gmail.com>
2019-07-12 21:39:11 -0400

    Added initial HTML page

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+// JavaScript Goes Here
+
+// Event listener for all button elements
+$("buttons").on("click", function() {
+    // Grab and store value of button
+    var selection = $(this).attr("data-value")
+});
+
+$("run-search").on("click", function()  {
+    // When 'Search' is clicked: Find and Post gifs
+    event.preventDefault();
+
+    var = 
+});
